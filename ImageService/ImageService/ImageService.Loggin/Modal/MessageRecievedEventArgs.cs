56a0d8eba1e6da37085e895f4eb65d8f83981a4f
Orenmc <orenmc@gmail.com>
2018-04-15 19:14:51 +0300

    final ex1

---
+++ ImageService/ImageService/ImageService.Loggin/Modal/MessageRecievedEventArgs.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageService
+{
+    /// <summary>
+    /// store the arguments of message
+    /// </summary>
+    class MessageRecievedEventArgs : EventArgs
+    {
+        public MessageTypeEnum Status { get; set; }
+        public string Message { get; set; }
+    }
+}
---
+++ ImageService/ImageService/ImageService/Commands/CloseHandleCommand.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageService
+{
+    /// <summary>
+    /// Close handle for directory
+    /// </summary>
+    class CloseHandleCommand : ICommand
+    {
+        /// <summary>
+        /// not relevant - must not execute.!
+        /// </summary>
+        public string Execute(string[] args, out bool result, out MessageTypeEnum type)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
