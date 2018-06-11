4f446c526a4d7c131b72d0dbb2b097e6c29d7117
SnirHazan <snirhazan30@gmail.com>
2018-06-11 13:09:04 +0300

    Add AlbumPhoto and display all the thumbnails

---
+++ 11.6.18 - יאללה לסיים/WebApplication2/WebApplication2/Controllers/ImageWebController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class ImageWebController : Controller
+    {
+        static ImageWeb ImageViewInfo = new ImageWeb();
+        public ImageWebController()
+        {
+        }
+
+        // GET: ImageWeb
+        public ActionResult WebImageFront()
+        {
+            ViewBag.Count = ImageViewInfo.NumberOfImages();
+            ViewBag.Status = ImageViewInfo.CheckConnection();
+            return View(ImageViewInfo);
+        }
+    }
+}
---
+++ 11.6.18 - יאללה לסיים/WebApplication2/WebApplication2/Controllers/PhotoController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class PhotoController : Controller
+    {
+        private static PhotoAlbum photos = new PhotoAlbum(); 
+        // GET: Photo
+        public ActionResult PhotoView()
+        {
+            return View(photos.PhotosAlbum);
+        }
+        public ActionResult DeletePhoto(string photoRelPath, string photoRelThumb)
+        {
+            ViewBag.path = photoRelPath;
+            return View();
+        }
+        public ActionResult ViewPhoto(string photoRelPath)
+        {
+            return View();
+        }
+    }
+}
